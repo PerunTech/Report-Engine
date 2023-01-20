a228ffd8edfe142e9b08a402c93774f7fd02f9d7
Kliment <k.kukoski@prtech.mk>
2023-01-20 11:46:27 +0100

    added some css improvements and startswithfilter

--- frontend/components/SideMenu.js
+++ frontend/components/SideMenu.js
@@ -13,8 +13,9 @@ const { ReactBootstrap } = elements;
 
 const SideMenu = (props) => {
     const [tables, setTables] = useState([])
-    const [childFields, setChildFields] = useState([])
     const [loading, setLoading] = useState(false)
+    const [inputArr, setinputArr] = useState([])
+    const [input, setInput] = useState('')
     useEffect(() => {
         setLoading(true)
         axios.get(`${window.server}/ReactElements/getTableData/${props.svSession}/SVAROG_TABLES/100000`).then((res) => {
@@ -27,12 +28,26 @@ const SideMenu = (props) => {
     }, [])
 
     const generateSideMenu = () => {
-        return tables.map(table => <div style={{ 'cursor': 'pointer' }} className={`${style['table-container']} ${table.opened ? style['opened'] : style['closed']}`} onClick={() => handleClick(table)} key={table['SVAROG_TABLES.OBJECT_ID']} id={table['SVAROG_TABLES.OBJECT_ID']}>
-            <div className={style['table-name']}><p>{table['SVAROG_TABLES.TABLE_NAME']}</p> <span>{table.opened ? icons.minus : icons.plus}</span></div>
-            <div className={style['field-container']}>
-                {table.opened && generateSideMenuChild(table)}
-            </div>
-        </div>)
+        if (input) {
+            if (inputArr.length > 0) {
+                return inputArr.map(table => <div style={{ 'cursor': 'pointer' }} className={`${style['table-container']} ${table.opened ? style['opened'] : style['closed']}`} onClick={() => handleClick(table)} key={table['SVAROG_TABLES.OBJECT_ID']} id={table['SVAROG_TABLES.OBJECT_ID']}>
+                    <div className={style['table-name']}><p>{table['SVAROG_TABLES.TABLE_NAME']}</p> <span>{table.opened ? icons.minus : icons.plus}</span></div>
+                    {table.opened && <div className={style['field-container']}>
+                        {generateSideMenuChild(table)}
+                    </div>}
+                </div>)
+            } else {
+                return <div className={style['empty-search-container']}><p>Нема пронајдени табели</p></div>
+            }
+
+        } else {
+            return tables.map(table => <div style={{ 'cursor': 'pointer' }} className={`${style['table-container']} ${table.opened ? style['opened'] : style['closed']}`} onClick={() => handleClick(table)} key={table['SVAROG_TABLES.OBJECT_ID']} id={table['SVAROG_TABLES.OBJECT_ID']}>
+                <div className={style['table-name']}><p>{table['SVAROG_TABLES.TABLE_NAME']}</p> <span>{table.opened ? icons.minus : icons.plus}</span></div>
+                {table.opened && <div className={style['field-container']}>
+                    {generateSideMenuChild(table)}
+                </div>}
+            </div>)
+        }
     }
 
     const handleClick = (table) => {
@@ -71,10 +86,25 @@ const SideMenu = (props) => {
         )
     }
 
+    const onChange = (e) => {
+        let tempArr = []
+        setInput(e.target.value.toUpperCase())
+        tables.forEach(element => {
+            if (element['SVAROG_TABLES.TABLE_NAME'].toUpperCase().startsWith(e.target.value.toUpperCase())) {
+                tempArr.push(element)
+            }
+        })
+        setinputArr(tempArr)
+        generateSideMenu(tempArr)
+    }
+
     return (
         <>
             {loading && <Loading />}
+            <div className={style['search-div']}><span>{icons.search}</span><input placeholder={'Внесете вредност за пребарување'} onChange={(e) => { onChange(e) }} type='string' /></div>
+
             {generateSideMenu()}
+
         </>
     );
 };
