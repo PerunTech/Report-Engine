2c9bb21304cdfef66b8b634a3a6f740abf0fef6e
Kliment <k.kukoski@prtech.mk>
2023-01-20 11:52:37 +0100

    format

--- frontend/components/SideMenu.js
+++ frontend/components/SideMenu.js
@@ -8,14 +8,13 @@ import {
 import style from "./../assets/ReportEngine.module.css"
 import { icons } from '../assets/svgHolder';
 const { useState, useEffect } = React;
-const { alertUser } = elements;
-const { ReactBootstrap } = elements;
 
 const SideMenu = (props) => {
     const [tables, setTables] = useState([])
     const [loading, setLoading] = useState(false)
     const [inputArr, setinputArr] = useState([])
     const [input, setInput] = useState('')
+
     useEffect(() => {
         setLoading(true)
         axios.get(`${window.server}/ReactElements/getTableData/${props.svSession}/SVAROG_TABLES/100000`).then((res) => {
@@ -39,7 +38,6 @@ const SideMenu = (props) => {
             } else {
                 return <div className={style['empty-search-container']}><p>Нема пронајдени табели</p></div>
             }
-
         } else {
             return tables.map(table => <div style={{ 'cursor': 'pointer' }} className={`${style['table-container']} ${table.opened ? style['opened'] : style['closed']}`} onClick={() => handleClick(table)} key={table['SVAROG_TABLES.OBJECT_ID']} id={table['SVAROG_TABLES.OBJECT_ID']}>
                 <div className={style['table-name']}><p>{table['SVAROG_TABLES.TABLE_NAME']}</p> <span>{table.opened ? icons.minus : icons.plus}</span></div>
@@ -76,7 +74,6 @@ const SideMenu = (props) => {
 
     }
 
-
     const generateSideMenuChild = (table) => {
         return table.childList.map(field => <div onClick={(e) => props.handleFieldClick(e, field)} key={field.key}>
 
@@ -102,9 +99,7 @@ const SideMenu = (props) => {
         <>
             {loading && <Loading />}
             <div className={style['search-div']}><span>{icons.search}</span><input placeholder={'Внесете вредност за пребарување'} onChange={(e) => { onChange(e) }} type='string' /></div>
-
             {generateSideMenu()}
-
         </>
     );
 };
