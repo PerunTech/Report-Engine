64c9c4216890d8e31e6594e69a82ac3d32febe1c
Kliment <k.kukoski@prtech.mk>
2023-02-27 11:29:42 +0100

    fix

--- frontend/components/MainContent/MainContent.js
+++ frontend/components/MainContent/MainContent.js
@@ -62,6 +62,7 @@ const MainContent = (props) => {
           default:
             return <select className={`custom-select ${style['main-content-select']}`} onChange={(e) => onChange(e, 'operator')} id={field.field_name} key={field.field_name}>
               <option value={'like'}>Like</option>
+              <option value={'equal'}>Equal</option>
               <option value={'startsWith'}>Starts with</option>
               <option value={'endsWith'}>Ends with</option>
             </select>
